d9fa502d5e37000b6b6a7c807790ba5433c87016
iamnotgoodatprogrammingplshelp <88812142+iamnotgoodatprogrammingplshelp@users.noreply.github.com>
2026-02-27 23:57:17 +0200

    Add getAllUsers function to retrieve user profiles

--- server-backend/controllers/user/profile.js
+++ server-backend/controllers/user/profile.js
@@ -1,1 +1,10 @@
-// Get user profile
+const User = require('../../models/User');
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};// Get user profile
