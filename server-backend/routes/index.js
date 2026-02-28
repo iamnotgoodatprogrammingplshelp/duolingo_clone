7ce5eb4a6092d1841a8ef291de5e7c84777e881f
iamnotgoodatprogrammingplshelp <88812142+iamnotgoodatprogrammingplshelp@users.noreply.github.com>
2026-02-28 16:12:59 +0200

    Update index.js

--- server-backend/routes/index.js
+++ server-backend/routes/index.js
@@ -1,1 +1,17 @@
 // Export all routes
+const express = require('express');
+const router = express.Router();
+
+const authRoutes = require('./auth');
+const userRoutes = require('./user');
+const budgetRoutes = require('./budgets');
+const transactionRoutes = require('./transactions');
+const analyticsRoutes = require('./analytics');
+
+router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
+router.use('/budgets', budgetRoutes);
+router.use('/transactions', transactionRoutes);
+router.use('/analytics', analyticsRoutes);
+
+module.exports = router;
